d2e6da017b704fc7a49fafe0c0549286084ec0cc
Jessica Salbert <jessicarsalbert@gmail.com>
2020-08-18 03:38:34 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -33,7 +33,7 @@ end
 def run(songs)
   puts "Please enter a command:"
   input = gets.strip
-  while input != "exit"
+  until input == "exit"
     if input == "list"
       list(songs)
     elsif input == "play"
